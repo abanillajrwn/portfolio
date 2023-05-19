da3ba7586bffe832a3c70accbfaa8a9e0bd50fdc
Jrwn <85285442+abanillajrwn@users.noreply.github.com>
2023-05-19 20:29:44 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+const discrip = document.querySelector('.proj-discrpt');
+const projImg = document.querySelector('.proj-img');
+const link = document.querySelector('.title');
+const expContent = document.querySelector('.exp-content');
+
+
+projImg.addEventListener('mouseleave', () =>{
+    discrip.style.display = "block";
+});
+
+//project one
+document.getElementById('content-one').addEventListener('mouseover', () => {
+    document.getElementById('go-proj-one').style.fontSize = '15px';
+    document.getElementById('go-proj-one').style.color = 'rgb(66, 176, 219)';
+
+})
+projImg.addEventListener('mouseover', () =>{
+    discrip.style.display = "none";
+
+});
+projImg.addEventListener('mouseleave', () =>{
+    discrip.style.display = "block";
+});
+
+document.getElementById('content-one').addEventListener('mouseleave', () => {
+    document.getElementById('go-proj-one').style.fontSize = '10px';
+    document.getElementById('go-proj-one').style.color = 'hsl(225, 100%, 95%)';
+    
+})
+
+document.getElementById('content-one').addEventListener("click", function(event) {
+    event.preventDefault(); // Prevents the default behavior of the link
+    window.open("https://github.com/abanillajrwn/mikmon-hotspot-template", "_blank"); // Opens the link in a new tab
+});
+
+//project two
+document.getElementById('content-two').addEventListener('mouseover', () => {
+    document.getElementById('go-proj-two').style.fontSize = '15px';
+    document.getElementById('go-proj-two').style.color = 'rgb(66, 176, 219)';
+
+})
+document.getElementById('content-two').addEventListener('mouseleave', () => {
+    document.getElementById('go-proj-two').style.fontSize = '10px';
+    document.getElementById('go-proj-two').style.color = 'hsl(225, 100%, 95%)';
+    
+})
+document.getElementById('content-two').addEventListener("click", function(event) {
+    event.preventDefault(); // Prevents the default behavior of the link
+    window.open("https://github.com/abanillajrwn/grading-system-proj", "_blank"); // Opens the link in a new tab
+});
+
+document.querySelector('.proj-two-img').addEventListener('mouseover', () => {
+    document.querySelector('.proj-two-discrpt').style.display = 'none';
+})
+document.querySelector('.proj-two-img').addEventListener('mouseout', () => {
+    document.querySelector('.proj-two-discrpt').style.display = 'block';
+})
